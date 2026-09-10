4aa651c243ec1c0231c6eefb3d122f215deff1b3
Miguel6273635 <132727142+Miguel6273635@users.noreply.github.com>
2026-09-10 17:38:35 -0600

    Sirve EquiposBloqueados desde la caché activa de API_DASH

--- webapp/model/EquiposBloqueadosService.js
+++ webapp/model/EquiposBloqueadosService.js
@@ -1,4 +1,6 @@
-sap.ui.define([], function () {
+sap.ui.define([
+    "mantenimiento/model/DashboardCacheODataModel"
+], function (DashboardCacheODataModel) {
     "use strict";
 
     var PAGE_SIZE = 1000;
@@ -115,6 +117,11 @@ sap.ui.define([], function () {
     }
 
     function getDashboardData(oModel) {
+        oModel = DashboardCacheODataModel.wrap(
+            oModel,
+            {},
+            ["catalogs","blocks","blockOrders","orders","blockEvents","resources"]
+        );
         if (
             !oModel ||
             typeof oModel.read !== "function"
