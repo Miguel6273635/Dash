750e3a7b7f66bfd0bdf2ffc5c6ef19380518bc45
Miguel6273635 <132727142+Miguel6273635@users.noreply.github.com>
2026-09-10 17:40:22 -0600

    Sirve PendientesDesbloqueo desde la caché activa de API_DASH

--- webapp/model/PendientesDesbloqueoService.js
+++ webapp/model/PendientesDesbloqueoService.js
@@ -1,8 +1,11 @@
 sap.ui.define([
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator"
-], function (Filter, FilterOperator) {
-    "use strict";
+,
+    "mantenimiento/model/DashboardCacheODataModel"
+], function (Filter, FilterOperator,
+    DashboardCacheODataModel
+) {    "use strict";
 
     var PAGE_SIZE = 1000;
     var MAX_PAGES = 50;
@@ -121,6 +124,11 @@ sap.ui.define([
     }
 
     function getDashboardData(oModel) {
+        oModel = DashboardCacheODataModel.wrap(
+            oModel,
+            {},
+            ["blockEvents","blocks","resources","catalogs"]
+        );
         if (!oModel || typeof oModel.read !== "function") {
             return Promise.reject(
                 new Error(
